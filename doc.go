b3eb54289ef4d80ac699d9ffcdaecb1f66ee12bc
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2017-11-29 15:41:12 -0600

    Provide initial doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+BiGr is a library representing Bioinformatics Grammar.
+
+This library defines biological data types abstractly by their functionality (interface) and how these abstract ypes relate to one another (functions) while saying nothing about how the concrete data is structured. The advantage of this approach is that concrete data can be restructured for efficiency while still providing a compile-time check that it is still a proper functional representation of its abstract type.
+*/
+package bigr
