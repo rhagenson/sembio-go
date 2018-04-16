3ba78ea6ab08e15ceedd94a258c982a0c7986b81
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-04-16 13:37:53 -0500

    Quick comment on use purpose of testing.go

--- interfaces/alphabet/testing.go
+++ interfaces/alphabet/testing.go
@@ -1,5 +1,10 @@
 package alphabet
 
+/*
+testing.go is a collection of test helpers which should wholly be used by people hoping to satisfy a
+bigr interface.
+*/
+
 // TestAlphabetProperLength is a test helpher to wrap a check for
 // an Alphbet implementation having the correct number of letters
 func TestAlphabetProperLength(a Alphabet, l int) bool {
