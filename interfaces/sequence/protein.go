0e7a67f48ed6eaf61e1409f471dd4f9e0734b681
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-04-16 14:18:42 -0500

    Add interfaces on top of protein representations

--- interfaces/sequence/protein.go
+++ interfaces/sequence/protein.go
@@ -3,3 +3,29 @@
 // license that can be found in the LICENSE file.
 
 package sequence
+
+import "bitbucket.org/rhagenson/bigr/interfaces/alphabet"
+
+// StrictProtein is any representation that uses the ProteinStrict Alphabet
+type StrictProtein interface {
+	Alphabet() alphabet.ProteinStrict
+}
+
+// GappedProtein is any representation that uses the ProteinGapped Alphabet
+type GappedProtein interface {
+	Alphabet() alphabet.ProteinGapped
+}
+
+// StrictProteinSequence is the combination of implementing both
+// StrictProtein and Sequence interfaces
+type StrictProteinSequence interface {
+	StrictProtein
+	Sequence
+}
+
+// GappedProteinSequence is the combination of implementing both
+// GappedProtein and Sequence interfaces
+type GappedProteinSequence interface {
+	GappedProtein
+	Sequence
+}
