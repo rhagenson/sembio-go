29bc2c1e35ac71973677f3e7ecc5d38477765a23
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-04-16 13:30:50 -0500

    Move Letter->fmt.Stringer to compile-time check

--- interfaces/alphabet/letter.go
+++ interfaces/alphabet/letter.go
@@ -1,6 +1,10 @@
 package alphabet
 
+import "fmt"
+
 // Letter is an arbitrary entry in an Alphabet (string allows multirune entries in the Alphabet)
 type Letter string
+
+var _ fmt.Stringer = Letter("")
 
 func (l Letter) String() string { return string(l) }
