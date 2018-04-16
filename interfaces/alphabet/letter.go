3b6a06a44da61802f78892bd365a4d08d9b5ba04
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-04-16 13:39:16 -0500

    Use new() rather than explicit initialization

--- interfaces/alphabet/letter.go
+++ interfaces/alphabet/letter.go
@@ -5,6 +5,6 @@ import "fmt"
 // Letter is an arbitrary entry in an Alphabet (string allows multirune entries in the Alphabet)
 type Letter string
 
-var _ fmt.Stringer = Letter("")
+var _ fmt.Stringer = new(Letter)
 
 func (l Letter) String() string { return string(l) }
