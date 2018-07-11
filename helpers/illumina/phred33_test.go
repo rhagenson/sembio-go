aa660d57a3b32606f39435160b1b1925b1c307fc
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-07-11 14:46:24 -0500

    Use numeric range rather than table test to check Phred33 q-score

--- helpers/illumina/phred33_test.go
+++ helpers/illumina/phred33_test.go
@@ -4,40 +4,13 @@ import (
 	"testing"
 )
 
-func TestQScore(t *testing.T) {
-	// Check the first 10 and last 10 valid ASCII characters for Phred+33
-	// Encoding
-	table := []struct {
-		in   byte
-		want uint8
-	}{
-		{'!', 0},
-		{'"', 1},
-		{'#', 2},
-		{'$', 3},
-		{'%', 4},
-		{'&', 5},
-		{'\'', 6},
-		{'(', 7},
-		{')', 8},
-		{'*', 9},
-		{'+', 10},
-		{'@', 31},
-		{'A', 32},
-		{'B', 33},
-		{'C', 34},
-		{'D', 35},
-		{'E', 36},
-		{'F', 37},
-		{'G', 38},
-		{'H', 39},
-		{'I', 40},
-	}
-
-	for _, tt := range table {
-		got, _ := Phred33QScore(tt.in)
-		if got != tt.want {
-			t.Errorf("Phred33QScore(%v) got %v; want: %v", tt.in, got, tt.want)
+func TestPhred33QScore(t *testing.T) {
+	want := uint8(0)
+	for c := MinPhred33; c <= MaxPhred33; c++ {
+		got, _ := Phred33QScore(c)
+		if got != want {
+			t.Errorf("Phred33QScore(%v) got %v; want: %v", c, got, want)
 		}
+		want++
 	}
 }
