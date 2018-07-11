34ed24f3b18b184c6b1b0f9f81bc81a6dbf2fb50
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-07-11 12:22:55 -0500

    Reduce code bloat and remove Kmer as example sequence in docs

--- sequence/interfaces.go
+++ sequence/interfaces.go
@@ -2,93 +2,57 @@ package sequence
 
 import "bitbucket.org/rhagenson/bigr/alphabet"
 
-// Interface is an abstract type defining the basic functionality of any biological sequence
-// (DNA, RNA, Protein, or some other series of Letters in a given alphabet)
-// A Interface is essentially an ordered list of elements where there is a Lenth(), each element has a Position(),
-// it is possible to look at a specific Range() of elements
+// Interface is the basic functionality of any biological sequence
+// (DNA, RNA, Protein, or other)
 type Interface interface {
-	// Length returns how many elements there are in the Sequence
+	// Length is the number of elements in the Interface
 	Length() uint
 
-	// Position returns the n-th element
+	// Position is the n-th element
 	Position(n uint) string
 
 	// Range returns elements from start (inclusive) to stop (exclusive)
 	Range(start, stop uint) string
 }
 
-// DNAstricter is any representation that uses the StrictDNAAlphabet, which
-// only has four (4) letters that are possible.
-type DNAstricter interface {
-	Alphabet() alphabet.DNAStrict
-}
-
-// DNAiupacer is any representation that uses the IUPACDNAAlphabet, which
-// has all sixteen (16) letters that are possible.
-type DNAiupacer interface {
-	Alphabet() alphabet.DNAIupac
-}
-
-// StrictDNASequence is the combination of implementing both
+// DnaStrictSequence is the combination of implementing both
 // StrictDNA and Sequence interfaces
-type StrictDNASequence interface {
-	DNAstricter
+type DnaStrictSequence interface {
 	Interface
+	Alphabet() *alphabet.DNAStrict
 }
 
-// IupacDNASequence is the combination of implementing both
+// DnaIupacSequence is the combination of implementing both
 // IupacDNA and Sequence interfaces
-type IupacDNASequence interface {
-	DNAiupacer
+type DnaIupacSequence interface {
 	Interface
+	Alphabet() *alphabet.DNAIupac
 }
 
-// RNAstricter is any representation that uses the StrictRNAAlphabet, which
-// only has four (4) letters that are possible.
-type RNAstricter interface {
-	Alphabet() alphabet.RNAStrict
-}
-
-// RNAiupacer is any representation that uses the IUPACRNAAlphabet, which
-// has all sixteen (16) letters that are possible.
-type RNAiupacer interface {
-	Alphabet() alphabet.RNAIupac
-}
-
-// StrictRNASequence is the combination of implementing both
-// StrictRNA and Sequence interfaces
-type StrictRNASequence interface {
-	RNAstricter
-	Interface
-}
-
-// IupacRNASequence is the combination of implementing both
-// IupacRNA and Sequence interfaces
-type IupacRNASequence interface {
-	RNAiupacer
-	Interface
-}
-
-// ProteinStricter is any representation that uses the ProteinStrict Alphabet
-type ProteinStricter interface {
-	Alphabet() alphabet.ProteinStrict
-}
-
-// ProteinGappeder is any representation that uses the ProteinGapped Alphabet
-type ProteinGappeder interface {
-	Alphabet() alphabet.ProteinGapped
-}
-
-// StrictProteinSequence is the combination of implementing both
+// ProteinStrictSequence is the combination of implementing both
 // StrictProtein and Sequence interfaces
-type StrictProteinSequence interface {
-	ProteinStricter
+type ProteinStrictSequence interface {
 	Interface
+	Alphabet() *alphabet.ProteinStrict
 }
 
-// GappedProteinSequence is the combination of implementing both
+// ProteinGappedSequence is the combination of implementing both
 // GappedProtein and Sequence interfaces
-type GappedProteinSequence interface {
-	ProteinGappeder
+type ProteinGappedSequence interface {
 	Interface
+	Alphabet() *alphabet.ProteinGapped
+}
+
+// RnaStrictSequence is the combination of implementing both
+// StrictRNA and Sequence interfaces
+type RnaStrictSequence interface {
+	Interface
+	Alphabet() *alphabet.RNAStrict
+}
+
+// RnaIupacSequence is the combination of implementing both
+// IupacRNA and Sequence interfaces
+type RnaIupacSequence interface {
+	Interface
+	Alphabet() *alphabet.RNAIupac
 }
