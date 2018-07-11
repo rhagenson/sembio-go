172b9d23afbc40c8cf34cb5935d35593b36f8a29
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-07-11 14:47:08 -0500

    Reduce code bloat by making sequence.Interface require Alphabet()

--- sequence/interfaces.go
+++ sequence/interfaces.go
@@ -13,46 +13,7 @@ type Interface interface {
 
 	// Range returns elements from start (inclusive) to stop (exclusive)
 	Range(start, stop uint) string
-}
 
-// DnaStrictSequence is the combination of implementing both
-// StrictDNA and Sequence interfaces
-type DnaStrictSequence interface {
-	Interface
-	Alphabet() *alphabet.DNAStrict
-}
-
-// DnaIupacSequence is the combination of implementing both
-// IupacDNA and Sequence interfaces
-type DnaIupacSequence interface {
-	Interface
-	Alphabet() *alphabet.DNAIupac
-}
-
-// ProteinStrictSequence is the combination of implementing both
-// StrictProtein and Sequence interfaces
-type ProteinStrictSequence interface {
-	Interface
-	Alphabet() *alphabet.ProteinStrict
-}
-
-// ProteinGappedSequence is the combination of implementing both
-// GappedProtein and Sequence interfaces
-type ProteinGappedSequence interface {
-	Interface
-	Alphabet() *alphabet.ProteinGapped
-}
-
-// RnaStrictSequence is the combination of implementing both
-// StrictRNA and Sequence interfaces
-type RnaStrictSequence interface {
-	Interface
-	Alphabet() *alphabet.RNAStrict
-}
-
-// RnaIupacSequence is the combination of implementing both
-// IupacRNA and Sequence interfaces
-type RnaIupacSequence interface {
-	Interface
-	Alphabet() *alphabet.RNAIupac
+	// Alphabet is the underlying valid character set
+	Alphabet() alphabet.Interface
 }
