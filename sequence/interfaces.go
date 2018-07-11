0c5504774a1ef738778a0b9f76d24fcce2952344
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-07-11 15:30:15 -0500

    Remove non-useful interfaces, need to implement as tests

--- sequence/interfaces.go
+++ sequence/interfaces.go
@@ -1,7 +1,5 @@
 package sequence
 
-import "bitbucket.org/rhagenson/bigr/alphabet"
-
 // Interface is the basic functionality of any biological sequence
 // (DNA, RNA, Protein, or other)
 type Interface interface {
@@ -13,24 +11,4 @@ type Interface interface {
 
 	// Range returns elements from start (inclusive) to stop (exclusive)
 	Range(start, stop uint) string
-}
-
-// Alphabeter provides a way to get at the sequence valid alphabet
-type Alphabeter interface {
-	Alphabet() alphabet.Interface
-}
-
-// Complementer provides a way to get at the sequence complement
-type Complementer interface {
-	Complement() Interface
-}
-
-// Reverser provides a way to get at the reverse sequence
-type Reverser interface {
-	Reverse() Interface
-}
-
-// RevComper provides a way to get at the reverse complement directly
-type RevComper interface {
-	RevComp() Interface
 }
