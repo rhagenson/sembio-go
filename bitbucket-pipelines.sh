bf9b3bef4bdb77a1e82aee9d53f16d81c4237dd6
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-07-19 16:05:23 -0500

    Fix typo: mdir to mkdir

--- bitbucket-pipelines.sh
+++ bitbucket-pipelines.sh
@@ -1,5 +1,5 @@
 BASE_PATH="${GOPATH}/src/bitbucket.org/${BITBUCKET_REPO_OWNER}"
-mdir -pv ${BASE_PATH}
+mkdir -pv ${BASE_PATH}
 export PKG_PATH="bitbucket.org/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}"
 
 git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
