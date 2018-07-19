b6514084dfa74d99a75b57c777e62d6a12ae2ac8
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-07-19 15:58:03 -0500

    Reduce number of setup ops and move all setup into script file

--- bitbucket-pipelines.sh
+++ bitbucket-pipelines.sh
@@ -1,5 +1,6 @@
 BASE_PATH="${GOPATH}/src/bitbucket.org/${BITBUCKET_REPO_OWNER}"
-mkdir -p ${BASE_PATH}
-export PKG_PATH="${BASE_PATH}/${BITBUCKET_REPO_SLUG}"
+PKG_PATH="${BASE_PATH}/${BITBUCKET_REPO_SLUG}"
+mkdir -pv ${PKG_PATH}
+cd ${PKG_PATH}
 
 git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
