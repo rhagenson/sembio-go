f71d0739cb68d84f934d885c735e58c5dabbedb6
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-07-19 16:10:00 -0500

    Realize that the conflict is RHagenson versus rhagenson

--- bitbucket-pipelines.sh
+++ bitbucket-pipelines.sh
@@ -1,5 +1,5 @@
 BASE_PATH="${GOPATH}/src/bitbucket.org/${BITBUCKET_REPO_OWNER}"
 mkdir -pv ${BASE_PATH}
-export PKG_PATH="bitbucket.org/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}"
+export PKG_PATH="bitbucket.org/rhagenson/${BITBUCKET_REPO_SLUG}"
 
 git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
