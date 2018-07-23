58b7641cfeb6ac012633c5f45ff720d260f11aee
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-07-23 14:19:10 -0500

    config Pipelines to use private repo
    Run go get -t -v -u ./... setup
    
    Remove ln -s to prevent symbolic copying
    
    Reduce number of setup ops and move all setup into script file
    
    Use PKG_PATH form <service>/<user>/<repo>
    
    Fix typo: mdir to mkdir
    
    Try -d flag to only download deps
    
    Realize that the conflict is RHagenson versus rhagenson

--- bitbucket-pipelines.sh
+++ bitbucket-pipelines.sh
@@ -1,4 +1,5 @@
 BASE_PATH="${GOPATH}/src/bitbucket.org/${BITBUCKET_REPO_OWNER}"
-mkdir -p ${BASE_PATH}
-export PKG_PATH="${BASE_PATH}/${BITBUCKET_REPO_SLUG}"
-ln -s ${PWD} ${PKG_PATH}
+mkdir -pv ${BASE_PATH}
+export PKG_PATH="bitbucket.org/rhagenson/${BITBUCKET_REPO_SLUG}"
+
+git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
