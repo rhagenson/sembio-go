5bfe10913a55ac17c34ad65ca2377fb039a9c574
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-07-26 16:36:10 -0500

    Wrap sequence creators to satisfy sequence.Generator interface

--- io/fasta/factories.go
+++ io/fasta/factories.go
@@ -8,22 +8,30 @@ import (
 
 // ReadDna reads in a FASTA file that should contain only valid DNA letters
 func ReadDna(r io.Reader) (Interface, error) {
-	return Read(r, sequence.NewDna)
+	return Read(r, func(s string) (sequence.Interface, error) {
+		return sequence.NewDna(s)
+	})
 }
 
 // ReadDnaIupac reads in a FASTA file that should contain
 // only valid IUPAC DNA letters
 func ReadDnaIupac(r io.Reader) (Interface, error) {
-	return Read(r, sequence.NewDnaIupac)
+	return Read(r, func(s string) (sequence.Interface, error) {
+		return sequence.NewDnaIupac(s)
+	})
 }
 
 // ReadRna reads in a FASTA file that should contain only valid DNA letters
 func ReadRna(r io.Reader) (Interface, error) {
-	return Read(r, sequence.NewRna)
+	return Read(r, func(s string) (sequence.Interface, error) {
+		return sequence.NewRna(s)
+	})
 }
 
 // ReadRnaIupac reads in a FASTA file that should contain
 // only valid IUPAC RNA letters
 func ReadRnaIupac(r io.Reader) (Interface, error) {
-	return Read(r, sequence.NewRnaIupac)
+	return Read(r, func(s string) (sequence.Interface, error) {
+		return sequence.NewRnaIupac(s)
+	})
 }
