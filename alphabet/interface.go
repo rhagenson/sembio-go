3753f95539a0addb025487c5e3ce6601a8cc3df8
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-07-26 14:22:02 -0500

    Not repeat the fmt.Stringer interface, reuse it

--- alphabet/interface.go
+++ alphabet/interface.go
@@ -1,16 +1,18 @@
 package alphabet
 
+import "fmt"
+
 // Interface is the abstract representation of a finite-letter alphabet
 // where validity of a given letter can be checked.
 type Interface interface {
+	// Must be stringable
+	fmt.Stringer
+
 	// Contains checks that the given elements are in the Alphabet
 	Contains(...string) []bool
 
 	// Length is the number of letters in the Alphabet
 	Length() int
-
-	// String will return the equivalent string denoting valid letters
-	String() string
 
 	// Width is the byte width of letters
 	// Zero (default int value) is the same as one to represent single-byte width
