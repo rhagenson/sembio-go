d8d424ad855fdd9764864887f1d255adf365e585
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-12-11 12:31:08 -0600

    Typo on 'nucleotide'

--- alphabet/constants.go
+++ alphabet/constants.go
@@ -28,25 +28,25 @@ const (
 	IupacLetters = "RYSWKM" + "BDHV" + "N"
 
 	// DnaLetters is the strict four-letter representation of DNA.
-	//	ATGC: Any of one nucelotide codes (i.e., 4 choose 1)
+	//	ATGC: Any of one nucleotide codes (i.e., 4 choose 1)
 	DnaLetters = "ATGC"
 
 	// DnaIupacLetters is the IUPAC representation of DNA.
 	//  -: Any of zero nucleotide codes (i.e., 4 choose 0)
-	// 	ATGC: Any of one nucelotide codes (i.e., 4 choose 1)
-	//	RYSWKM: Any of two nucelotide codes (i.e., 4 choose 2)
+	// 	ATGC: Any of one nucleotide codes (i.e., 4 choose 1)
+	//	RYSWKM: Any of two nucleotide codes (i.e., 4 choose 2)
 	//	BDHV: Any of three nucleotide codes (i.e., 4 choose 3)
 	//  N: Any of four nucleotide codes (i.e., 4 choose 4)
 	DnaIupacLetters = DnaLetters + IupacLetters + GapLetter
 
 	// RnaLetters is the strict four-letter representation of DNA.
-	//	AUGC: Any of one nucelotide codes (i.e., 4 choose 1)
+	//	AUGC: Any of one nucleotide codes (i.e., 4 choose 1)
 	RnaLetters = "AUGC"
 
 	// RnaIupacLetters is the IUPAC representation of RNA.
 	//  -: Any of zero nucleotide codes (i.e., 4 choose 0)
-	//  AUGC: Any of one nucelotide codes (i.e., 4 choose 1)
-	//	RYSWKM: Any of two nucelotide codes (i.e., 4 choose 2)
+	//  AUGC: Any of one nucleotide codes (i.e., 4 choose 1)
+	//	RYSWKM: Any of two nucleotide codes (i.e., 4 choose 2)
 	//	BDHV: Any of three nucleotide codes (i.e., 4 choose 3)
 	//  N: Any of four nucleotide codes (i.e., 4 choose 4)
 	RnaIupacLetters = RnaLetters + IupacLetters + GapLetter
