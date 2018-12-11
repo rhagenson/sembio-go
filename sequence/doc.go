28f3270985fc8cee6ae913501b5cfeaca579b06e
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-12-11 14:34:22 -0600

    Shorten synopsis

--- sequence/doc.go
+++ sequence/doc.go
@@ -1,5 +1,4 @@
 /*
-Package sequence defines the abstract interface for a biological
-sequence -- be it DNA, RNA, Protein, or otherwise.
+Package sequence defines the abstract interface for a biological sequence.
 */
 package sequence
