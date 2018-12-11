908243f153b80f0390c6b9ea9c0091ecfd258940
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-12-11 14:26:47 -0600

    Statisfy the correct Interface

--- sequence/persistent/sequence_test.go
+++ sequence/persistent/sequence_test.go
@@ -1,5 +1,7 @@
 package persistent
 
-var _ Interface = new(Struct)
+import "bitbucket.org/rhagenson/bio/sequence"
+
+var _ sequence.Interface = new(Struct)
 var _ Wither = new(Struct)
 var _ Validator = new(Struct)
