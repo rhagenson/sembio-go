9e63d66d34704b9b126baa270742655cdf2b76ba
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-12-13 10:38:40 -0600

    Add docstring for public FastaHeaderPrefix

--- io/fasta/constants.go
+++ io/fasta/constants.go
@@ -1,5 +1,7 @@
 package fasta
 
 const (
+	// FastaHeaderPrefix is the character used by FASTA format to indicate a header line
+	// Any lines NOT prefixed with FastaHeaderPrefix are considered body/sequence lines.
 	FastaHeaderPrefix = '>'
 )
