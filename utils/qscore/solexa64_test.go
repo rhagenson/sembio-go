cdcacee3dbe2be4c35e5d158f248037e671729a7
Ryan A. Hagenson <Ryan.Hagenson@gmail.com>
2018-12-12 19:56:16 -0600

    Fix copypaste error to yield 100% test coverage

--- utils/qscore/solexa64_test.go
+++ utils/qscore/solexa64_test.go
@@ -20,24 +20,24 @@ func TestSolexa64(t *testing.T) {
 
 func TestSolexa64Errors(t *testing.T) {
 	t.Run("Below minimum errors out", func(t *testing.T) {
-		for c := byte(0); c < qscore.MinPhred33; c++ {
-			got, err := qscore.IlluminaPhred33(c)
+		for c := byte(0); c < qscore.MinSolexa64; c++ {
+			got, err := qscore.Solexa64(c)
 			if err == nil {
-				t.Errorf("SangerPhred33(%v) should fail, got nil error", c)
+				t.Errorf("Solexa64(%v) should fail, got nil error", c)
 			}
 			if got != 0 {
-				t.Errorf("SangerPhred33(%v) should fail, got %v value", c, got)
+				t.Errorf("Solexa64(%v) should fail, got %v value", c, got)
 			}
 		}
 	})
 	t.Run("Above maximum errors out", func(t *testing.T) {
-		for c := byte(qscore.MaxSangerPhred33 + 1); c < byte(math.MaxUint8)/2; c++ {
-			got, err := qscore.SangerPhred33(c)
+		for c := byte(qscore.MaxSolexa64 + 1); c < byte(math.MaxUint8)/2; c++ {
+			got, err := qscore.Solexa64(c)
 			if err == nil {
-				t.Errorf("SangerPhred33(%v) should fail, got nil error", c)
+				t.Errorf("Solexa64(%v) should fail, got nil error", c)
 			}
 			if got != 0 {
-				t.Errorf("SangerPhred33(%v) should fail, got %v value", c, got)
+				t.Errorf("Solexa64(%v) should fail, got %v value", c, got)
 			}
 		}
 	})
