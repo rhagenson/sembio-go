d8e64ff8e07a61a3c0a9f4efe5cc2db9d095b5be
Ryan A. Hagenson <Ryan.Hagenson@gmail.com>
2019-04-17 21:41:39 -0500

    Use map for constant lookup

--- alphabet/alphabet.go
+++ alphabet/alphabet.go
@@ -1,36 +1,42 @@
 package alphabet
 
-import (
-	"bytes"
-)
-
 // Alphabet is a collection of letters
 type Alphabet struct {
-	letters []byte
+	chars map[byte]struct{}
 }
 
 // New is an Alphabet generator
 func New(letters string) *Alphabet {
+	set := make(map[byte]struct{}, len(letters))
+	for _, b := range []byte(letters) {
+		set[b] = struct{}{}
+	}
 	return &Alphabet{
-		letters: []byte(letters),
+		chars: set,
 	}
 }
 
 // Length is numbers of letters in the Alphabet
 func (a Alphabet) Length() int {
-	return len(a.letters)
+	return len(a.chars)
 }
 
-// Contains confirms whether an array of potential letters are in the Alphabet
+// Contains confirms whether a potential letters are in the Alphabet
 func (a Alphabet) Contains(letters ...byte) []bool {
 	found := make([]bool, len(letters))
 	for i, l := range letters {
-		found[i] = bytes.IndexByte(a.letters, l) != -1
+		if _, ok := a.chars[l]; ok {
+			found[i] = true
+		}
 	}
 	return found
 }
 
 // String generates a stringified copy of the Alphabet
 func (a Alphabet) String() string {
-	return string(a.letters)
+	letters := make([]byte, 0, len(a.chars))
+	for k := range a.chars {
+		letters = append(letters, k)
+	}
+	return string(letters)
 }
