9381379471579d66c0887f5ac83ca8f962b06f5e
Ryan A. Hagenson <Ryan.Hagenson@gmail.com>
2019-04-17 21:42:04 -0500

    Fix to use 'and' not 'or'

--- alphabet/doc.go
+++ alphabet/doc.go
@@ -1,4 +1,4 @@
 /*
-Package alphabet is a collection of defined biological alphabets such as DNA, RNA, or Protein
+Package alphabet is a collection of defined biological alphabets such as DNA, RNA, and Protein
 */
 package alphabet
