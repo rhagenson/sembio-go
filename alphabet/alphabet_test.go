d77f66b40fbb8a4432b4955625a05ade27c1ef6f
Ryan A. Hagenson <Ryan.Hagenson@gmail.com>
2019-04-17 21:47:51 -0500

    New test helper names and select random ASCII letter

--- alphabet/alphabet_test.go
+++ alphabet/alphabet_test.go
@@ -3,6 +3,7 @@ package alphabet_test
 import (
 	"bytes"
 	"fmt"
+	"math/rand"
 	"testing"
 
 	"bitbucket.org/rhagenson/bio/alphabet"
@@ -11,10 +12,10 @@ import (
 func TestTesting(t *testing.T) {
 	t.Run("IsExpectedLength", func(t *testing.T) {
 		a := alphabet.New("N")
-		t.Run("Success", alphabet.IsExpectedLength(a, 1))
+		t.Run("Success", alphabet.TestIsExpectedLength(a, 1))
 		t.Run("Failure", func(t *testing.T) {
 			var t2 = new(testing.T)
-			alphabet.IsExpectedLength(a, 2)(t2)
+			alphabet.TestIsExpectedLength(a, 2)(t2)
 			if !t2.Failed() {
 				t.Errorf("Failure case incorrectly passed.")
 			}
@@ -22,28 +23,30 @@ func TestTesting(t *testing.T) {
 	})
 	t.Run("HasExpectedLetter", func(t *testing.T) {
 		a := alphabet.New("N")
-		t.Run("Success", alphabet.HasExpectedLetter(a, 'N'))
+		t.Run("Success", alphabet.TestHasExpectedLetter(a, 'N'))
 		t.Run("Failure", func(t *testing.T) {
 			var t2 = new(testing.T)
-			alphabet.HasExpectedLetter(a, 'X')(t2)
+			alphabet.TestHasExpectedLetter(a, 'X')(t2)
 			if !t2.Failed() {
 				t.Errorf("Failure case incorrectly passed.")
 			}
 		})
 	})
 	t.Run("NotLetters", func(t *testing.T) {
-		letter := byte('A') // TODO: Replace with a random ASCII character
+		// Chooses random ASCII letter in range from
+		// minimum position (A) to maximum position (z)
+		letter := byte(rand.Intn('z'-'A') + 'A')
 		t.Run("Success", func(t *testing.T) {
-			if bytes.IndexByte(alphabet.NotLetters([]byte{letter}), letter) != -1 {
+			if bytes.IndexByte(alphabet.TestExcludesLetters([]byte{letter}), letter) != -1 {
 				t.Errorf(fmt.Sprintf("%q was found when it should not have been", letter))
 			}
 		})
 		t.Run("Failure", func(t *testing.T) {
 			nextLetter := letter
-			if letter += 1; letter > 'z' {
-				nextLetter = 'a'
+			if nextLetter += 1; nextLetter > 'z' {
+				nextLetter = 'A' // Wrap to ASCII start
 			}
-			if bytes.IndexByte(alphabet.NotLetters([]byte{letter}), nextLetter) == 1 {
+			if bytes.IndexByte(alphabet.TestExcludesLetters([]byte{letter}), nextLetter) == -1 {
 				t.Errorf("Failure case incorrectly passed.")
 			}
 		})
@@ -53,8 +56,8 @@ func TestTesting(t *testing.T) {
 func TestDna(t *testing.T) {
 	var a alphabet.Interface = alphabet.Dna
 	letters := []byte("ATGC")
-	notLetters := alphabet.NotLetters(letters)
-	t.Run("Correct length", alphabet.IsExpectedLength(a, 4))
+	notLetters := alphabet.TestExcludesLetters(letters)
+	t.Run("Correct length", alphabet.TestIsExpectedLength(a, 4))
 	t.Run("Expected letters", func(t *testing.T) {
 		for i, v := range a.Contains(letters...) {
 			t.Run(fmt.Sprintf("Contains %q", letters[i]), func(t *testing.T) {
@@ -78,9 +81,9 @@ func TestDna(t *testing.T) {
 func TestDnaIupac(t *testing.T) {
 	var a alphabet.Interface = alphabet.DnaIupac
 	letters := []byte("ATGC" + "RYSWKM" + "BDHVN")
-	notLetters := alphabet.NotLetters(letters)
-	t.Run("Correct length", alphabet.IsExpectedLength(a, 16))
-	t.Run("Has gap", alphabet.HasExpectedLetter(a, '-'))
+	notLetters := alphabet.TestExcludesLetters(letters)
+	t.Run("Correct length", alphabet.TestIsExpectedLength(a, 16))
+	t.Run("Has gap", alphabet.TestHasExpectedLetter(a, '-'))
 	t.Run("Expected letters", func(t *testing.T) {
 		for i, v := range a.Contains(letters...) {
 			t.Run(fmt.Sprintf("Contains %q", letters[i]), func(t *testing.T) {
@@ -104,8 +107,8 @@ func TestDnaIupac(t *testing.T) {
 func TestRna(t *testing.T) {
 	var a alphabet.Interface = alphabet.Rna
 	letters := []byte("AUGC")
-	notLetters := alphabet.NotLetters(letters)
-	t.Run("Correct length", alphabet.IsExpectedLength(a, 4))
+	notLetters := alphabet.TestExcludesLetters(letters)
+	t.Run("Correct length", alphabet.TestIsExpectedLength(a, 4))
 	t.Run("Expected letters", func(t *testing.T) {
 		for i, v := range a.Contains(letters...) {
 			t.Run(fmt.Sprintf("Contains %q", letters[i]), func(t *testing.T) {
@@ -129,9 +132,9 @@ func TestRna(t *testing.T) {
 func TestRnaIupac(t *testing.T) {
 	var a alphabet.Interface = alphabet.RnaIupac
 	letters := []byte("AUGC" + "RYSWKM" + "BDHVN")
-	notLetters := alphabet.NotLetters(letters)
-	t.Run("Correct length", alphabet.IsExpectedLength(a, 16))
-	t.Run("Has gap", alphabet.HasExpectedLetter(a, '-'))
+	notLetters := alphabet.TestExcludesLetters(letters)
+	t.Run("Correct length", alphabet.TestIsExpectedLength(a, 16))
+	t.Run("Has gap", alphabet.TestHasExpectedLetter(a, '-'))
 	t.Run("Expected letters", func(t *testing.T) {
 		for i, v := range a.Contains(letters...) {
 			t.Run(fmt.Sprintf("Contains %q", letters[i]), func(t *testing.T) {
@@ -155,8 +158,8 @@ func TestRnaIupac(t *testing.T) {
 func TestProtein(t *testing.T) {
 	var a alphabet.Interface = alphabet.Protein
 	letters := []byte("ACDEFGHIKLMNPQRSTVWY")
-	notLetters := alphabet.NotLetters(letters)
-	t.Run("Correct length", alphabet.IsExpectedLength(a, 20))
+	notLetters := alphabet.TestExcludesLetters(letters)
+	t.Run("Correct length", alphabet.TestIsExpectedLength(a, 20))
 	t.Run("Expected letters", func(t *testing.T) {
 		for i, v := range a.Contains(letters...) {
 			t.Run(fmt.Sprintf("Contains %q", letters[i]), func(t *testing.T) {
@@ -180,9 +183,9 @@ func TestProtein(t *testing.T) {
 func TestProteinGapped(t *testing.T) {
 	var a alphabet.Interface = alphabet.ProteinGapped
 	letters := []byte("ACDEFGHIKLMNPQRSTVWY")
-	notLetters := alphabet.NotLetters(letters)
-	t.Run("Correct length", alphabet.IsExpectedLength(a, 21))
-	t.Run("Has gap", alphabet.HasExpectedLetter(a, '-'))
+	notLetters := alphabet.TestExcludesLetters(letters)
+	t.Run("Correct length", alphabet.TestIsExpectedLength(a, 21))
+	t.Run("Has gap", alphabet.TestHasExpectedLetter(a, '-'))
 	t.Run("Expected letters", func(t *testing.T) {
 		for i, v := range a.Contains(letters...) {
 			t.Run(fmt.Sprintf("Contains %q", letters[i]), func(t *testing.T) {
