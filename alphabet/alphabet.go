4ac953facc68779d61168bc8bab3ce10d20ceeff
Ryan A. Hagenson <Ryan.Hagenson@gmail.com>
2019-04-20 14:08:40 -0500

    Make alphabet match pkg.Interface/pkg.Struct pattern

--- alphabet/alphabet.go
+++ alphabet/alphabet.go
@@ -1,28 +1,28 @@
 package alphabet
 
-// Alphabet is a collection of letters
-type Alphabet struct {
+// Struct is a collection of letters
+type Struct struct {
 	chars map[byte]struct{}
 }
 
-// New is an Alphabet generator
-func New(letters string) *Alphabet {
+// New is an Struct generator
+func New(letters string) *Struct {
 	set := make(map[byte]struct{}, len(letters))
 	for _, b := range []byte(letters) {
 		set[b] = struct{}{}
 	}
-	return &Alphabet{
+	return &Struct{
 		chars: set,
 	}
 }
 
 // Length is numbers of letters in the Alphabet
-func (a Alphabet) Length() int {
+func (a Struct) Length() int {
 	return len(a.chars)
 }
 
 // Contains confirms whether a potential letters are in the Alphabet
-func (a Alphabet) Contains(letters ...byte) []bool {
+func (a Struct) Contains(letters ...byte) []bool {
 	found := make([]bool, len(letters))
 	for i, l := range letters {
 		if _, ok := a.chars[l]; ok {
@@ -33,7 +33,7 @@ func (a Alphabet) Contains(letters ...byte) []bool {
 }
 
 // String generates a stringified copy of the Alphabet
-func (a Alphabet) String() string {
+func (a Struct) String() string {
 	letters := make([]byte, 0, len(a.chars))
 	for k := range a.chars {
 		letters = append(letters, k)
